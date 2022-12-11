599be17b7aa8d98a168afb9a81c4a28adfe73964
Syarifhidayatullah11 <srfhidayatullah0711@gmail.com>
2022-12-11 14:30:03 +0700

    commit dashbord

--- application/views/index/index.php
+++ application/views/index/index.php
@@ -9,12 +9,10 @@
     <title>Dashboard Template · Bootstrap v5.2</title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/dashboard/">
-
     
 
     
-
-<link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo base_url('public') ?>/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
     <style>
       .bd-placeholder-img {
@@ -316,7 +314,7 @@
 </div>
 
 
-    <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="<?=base_url('public')?>/lib/bootstrap/js/bootstrap.bundle.min.js"></script>
 
       <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script><script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js" integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha" crossorigin="anonymous"></script><script src="dashboard.js"></script>
   </body>
