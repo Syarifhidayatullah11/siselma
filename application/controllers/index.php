a33bc30281a8948b1d51dde38f6b2e702af76903
Syarifhidayatullah11 <srfhidayatullah0711@gmail.com>
2022-12-11 07:14:45 +0700

    progres minggu

--- application/controllers/index.php
+++ application/controllers/index.php
@@ -5,8 +5,9 @@ class Index extends CI_Controller
 {
 
 	
-	      public function index()
+	public function index()
 	        {
-		$this->load->view('index/index');
+				$this->load->view('index/index');
 	        }
+
 }
